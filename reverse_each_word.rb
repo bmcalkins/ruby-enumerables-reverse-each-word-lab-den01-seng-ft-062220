e43b00c3b0a71ea83c23918f7f2e46c628ad24f9
Bradley Calkins <b.raders1013@gmail.com>
2020-06-18 04:02:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence1 = "Hello there, and how are you?")
   new_array = sentence1.split(" ") 
   new_array.map { |word|
     word_array = word.split("")
-       return word_array.reverse().join("")
+       word_array.reverse().join("")
   }.join(" ")
 end 
