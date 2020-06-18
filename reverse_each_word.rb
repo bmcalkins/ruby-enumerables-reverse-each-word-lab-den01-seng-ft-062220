7622c27ea796340dc84f2f1bfdcef5a0ff2d889a
Bradley Calkins <b.raders1013@gmail.com>
2020-06-18 03:50:14 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(sentence1 = "Hello there, and how are you?")
+new_array = sentence1.split() 
+conformed_array = []
+ new_array.each do |index|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+ end  
